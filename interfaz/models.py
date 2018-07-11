38c32463fe3e8c06797bc81b9838161fbf4ed488
Edwar Yepez <vemazutiel@gmail.com>
2018-07-10 21:16:11 -0400

    commit para hacer pull

--- interfaz/models.py
+++ interfaz/models.py
@@ -1,3 +1,22 @@
 from django.db import models
-
+from django.contrib.auth.models import User
 # Create your models here.
+
+
+class UserProfile(models.Model):
+	
+	ROLES = (
+		('Estudiante', 'Estudiante'),
+		('Profesor', 'Profesor'),
+		('Representante','Representante'),
+	)
+
+	tipo_usuario = models.CharField('Tipo de usuario',max_length=50,null=True,choices=ROLES)
+	user = models.OneToOneField(
+		User,
+		on_delete = models.CASCADE,
+		default='userprofile'
+	)
+
+	def __str__(self):
+		return self.user.email
--- interfaz/views.py
+++ interfaz/views.py
@@ -1,3 +1,21 @@
 from django.shortcuts import render
-
+from django.views.generic import TemplateView
 # Create your views here.
+class HomeView(TemplateView):
+	template_name='index.html'
+	
+	#  Allows getting the context necessary to render the view
+    #
+    #  @date [13/08/2017]
+    #
+    #  @author [Veronica Mazutiel]
+    #
+    #  @returns Dict containing the form to login
+    #
+	
+	def get_context_data(self, **kwargs):
+		context = super(HomeView, self).get_context_data(**kwargs)
+		#context['form'] = LoginForm()
+		#context['baseURL'] = url.baseURL()
+		#context['pagename'] = 'home'
+		return context
